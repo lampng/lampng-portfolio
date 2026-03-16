2cfc277fcfaedf8359d5fa3752bdab484534cc4a
Phan Nguyen Gia Lam <lampngia@gmail.com>
2026-03-16 21:39:46 +0700

    add components + config url local

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,107 @@
-import Image from "next/image";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { ArrowRight, Circle, MessageCircle } from 'lucide-react';
+import Image from 'next/image';
+
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    return (
+        <main className="min-h-screen max-w-2xl mx-auto  py-12 px-6  sm:py-24">
+            <section className="max-w-2xl mx-auto">
+                <div className="gap-2 flex justify-between">
+                    <div className="flex-col flex flex-1 space-y-3">
+                        <div className="flex">
+                            <span className="inline-block text-5xl font-bold tracking-tighter sm:text-6xl xl:text-6xl/none">
+                                Hi, I&apos;m Lâm 👋
+                            </span>
+                        </div>
+                        <div className="flex">
+                            <span className="inline-block max-w-[600px] md:text-xl">
+                                Fullstack Developer focused on building modern web applications. I enjoy turning ideas
+                                into scalable products using technologies like React.js, Next.js, Node.js, Nest.js and
+                                modern databases.
+                            </span>
+                        </div>
+                    </div>
+                    <div className="w-30 h-30 relative overflow-hidden rounded-full center">
+                        <Image src="/me.jpg" alt="avatar" fill className="object-cover" />
+                    </div>
+                </div>
+            </section>
+            {/* About section */}
+            <section about="about" className="mt-10 space-y-3">
+                <h2 className="font-bold text-2xl">About</h2>
+                <p className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert ">
+                    My main focus is full-stack web development using modern JavaScript technologies. I enjoy working
+                    across the entire stack — from designing frontend interfaces to building scalable backend services.
+                    My goal is to become a strong software engineer who can design systems, write clean maintainable
+                    code, and build products that people love to use.
+                </p>
+            </section>
+            {/* Skills section */}
+            <section about="about" className="mt-10">
+                <h2 className="font-bold text-2xl">Technologies</h2>
+                <Accordion type="single" collapsible>
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger className="flex items-center gap-4">
+                            <div className="flex items-center gap-4">
+                                <div className="w-8 h-8 relative rounded-full overflow-hidden">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                        <path d="M64.5 320C64.5 461 178.1 575 319.1 575C461.1 575 575.1 461 575.1 320C575.1 180 461.1 66.1 319.1 66.1C178.1 66.1 64.5 180 64.5 320zM256.1 214L256.1 427L213.1 427L213.1 214L256.1 214zM298.1 214L426.1 214L426.1 257L298.1 257L298.1 214zM426.1 299L426.1 342L298.1 342L298.1 299L426.1 299zM298.1 384L426.1 384L426.1 427L298.1 427L298.1 384z" />
+                                    </svg>
+                                </div>
+                                <div className="text-left">
+                                    <p className="text-sm font-bold">Front-End</p>
+                                </div>
+                            </div>
+                        </AccordionTrigger>
+
+                        <AccordionContent className="pl-16 text-muted-foreground">
+                            <span className="font-bold underline">HTML</span> -{' '}
+                            <span className="font-bold underline">CSS</span> /{' '}
+                            <span className="font-bold underline">TailwindCSS</span> -{' '}
+                            <span className="font-bold underline">Shadcn</span> -{' '}
+                            <span className="font-bold underline">JavaScript</span> -{' '}
+                            <span className="font-bold underline">React.js</span> -{' '}
+                            <span className="font-bold underline">Next.js</span>
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger className="flex items-center gap-4">
+                            <div className="flex items-center gap-4">
+                                <div className="w-8 h-8 relative rounded-full overflow-hidden">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                        <path d="M544 269.8C529.2 279.6 512.2 287.5 494.5 293.8C447.5 310.6 385.8 320 320 320C254.2 320 192.4 310.5 145.5 293.8C127.9 287.5 110.8 279.6 96 269.8L96 352C96 396.2 196.3 432 320 432C443.7 432 544 396.2 544 352L544 269.8zM544 192L544 144C544 99.8 443.7 64 320 64C196.3 64 96 99.8 96 144L96 192C96 236.2 196.3 272 320 272C443.7 272 544 236.2 544 192zM494.5 453.8C447.6 470.5 385.9 480 320 480C254.1 480 192.4 470.5 145.5 453.8C127.9 447.5 110.8 439.6 96 429.8L96 496C96 540.2 196.3 576 320 576C443.7 576 544 540.2 544 496L544 429.8C529.2 439.6 512.2 447.5 494.5 453.8z" />
+                                    </svg>
+                                </div>
+                                <div className="text-left">
+                                    <p className="text-sm  font-bold">Backend</p>
+                                </div>
+                            </div>
+                        </AccordionTrigger>
+
+                        <AccordionContent className="pl-10 text-muted-foreground">
+                            <span className="font-bold underline">Node.js</span> -{' '}
+                            <span className="font-bold underline">Express</span> /{' '}
+                            <span className="font-bold underline">NestJS</span> -{' '}
+                            <span className="font-bold underline">PostgreSQL</span> -{' '}
+                            <span className="font-bold underline">MongoDB</span> -{' '}
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </section>
+            {/* Project section */}
+            <section about="Project" className="mt-10">
+                <div className="flex flex-col items-center justify-center text-center space-y-3">
+                    <Button className="pointer-events-none">My Project</Button>
+                    <h1 className="text-2xl font-bold tracking-tighter sm:text-3xl xl:text-4xl/none">
+                        Check out my latest project
+                    </h1>
+                    <span className="text-muted-foreground">Here are some of the projects I&apos;ve worked on.</span>
+                </div>
+            </section>
+        </main>
+    );
 }
